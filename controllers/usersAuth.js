5b744f099f7d24a5cab6aeba8d6989ce264d7393
Talha Mughal <talhamughal600@gmail.com>
2019-03-03 18:36:15 +0500

    Added new user Controller to register new user

---
+++ controllers/usersAuth.js
@@ -0,0 +1,36 @@
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+// User Model
+const User = require("../models/User");
+
+// POST Route, User Registration
+exports.userRegister = (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: 200,
+        r: "pg",
+        d: "mm"
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: avatar
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+};
--- routes/api/usersAuth.js
+++ routes/api/usersAuth.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
+const userAuthController = require("../../controllers/usersAuth");
+
 const router = express.Router();
 
 router.get("/test", (req, res) => {
   res.json({ message: "users works" });
 });
+
+router.post("/register", userAuthController.userRegister);
 
 module.exports = router;
