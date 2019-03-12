8c63067c3d567b38c4721eb106370977bbf6b5c1
Talha Mughal <talhamughal600@gmail.com>
2019-03-12 11:27:41 +0500

    profile complete

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import NotFound from "./components/not-found/NotFound";
 
 class App extends Component {
   render() {
@@ -54,6 +55,7 @@ class App extends Component {
               component={AddEducation}
             />
           </Switch>
+          <Route exact path='/not-found' component={NotFound} />
           <Footer />
         </div>
       </BrowserRouter>
